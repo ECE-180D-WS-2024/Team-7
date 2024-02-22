a79f66a4cfd9a5086f1aa138b66ee2926a46d2e4
nturner0 <nturner1@g.ucla.edu>
2024-02-22 15:41:35 -0800

    Updated speech_recog with additional TODOs, and implemented transcription processing to a single-word command string

--- speech_recog.py
+++ speech_recog.py
@@ -1,9 +1,16 @@
 import speech_recognition as sr
 
+#Helper function to extract keywords from a list
+#Takes in a list, the words to parse, and a list of keywords
+#Outputs a list containing only the keywords in the original list
+def extract_keywords(word_list, keywords):
+    extracted_keywords = [word for word in word_list if any(keyword in word for keyword in keywords)]
+    return extracted_keywords
+
 #Takes in a recognizer and microphone object, and a list of strings of valid commands
 #Returns a response dictionary
 #If the transcription was successful, detect whether it is a valid commmand
-def recognize_speech_from_mic(recognizer, microphone):
+def recognize_speech_from_mic(recognizer, microphone, available_commands):
     """Transcribe speech from recorded from `microphone`.
 
     Returns a dictionary with three keys:
@@ -26,7 +33,7 @@ def recognize_speech_from_mic(recognizer, microphone):
     # from the microphone
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.listen(source, phrase_time_limit=1)
+        audio = recognizer.listen(source, phrase_time_limit=2)
 
     # set up the response object
     response = {
@@ -37,12 +44,15 @@ def recognize_speech_from_mic(recognizer, microphone):
 
     #TODO: process the transcription, and remove anything that isnt a valid input
     #TODO: count the occurance of each valid commands, and return only a one word string of the most frequent command
+    #TODO: could try starting listening at a exact given time, instead of when the microphone energy passes the threshold
+    #TODO: look into Snowboy to use a hotword instead of the recgonizer. since we only have one word commands, this may be a useful alternative.
 
     # try recognizing the speech in the recording
     # if a RequestError or UnknownValueError exception is caught,
     #     update the response object accordingly
+    raw_words = None
     try:
-        response["transcription"] = recognizer.recognize_google(audio)
+        raw_words = recognizer.recognize_google(audio)
     except sr.RequestError:
         # API was unreachable or unresponsive
         response["success"] = False
@@ -51,8 +61,18 @@ def recognize_speech_from_mic(recognizer, microphone):
         # speech was unintelligible
         response["error"] = "Unable to recognize speech"
 
-    return response
+    #process the transcription
+    if (raw_words):
+        #remove all words except keyword commands
+        words_parsed = extract_keywords(raw_words.split(), available_commands)
+        #take the mode of the commands, and replace transcription with the command
+        if (words_parsed):
+            response["transcription"] = max(set(words_parsed), key=words_parsed.count)
+        else:
+            #response["error"] = "Words detected, but no commands. Original input was:\n" + raw_words
+            response["error"] = "Words detected, but no commands. Parsed input was:\n" + str(words_parsed)
 
+    return response
 
 if __name__ == "__main__":
 
@@ -65,9 +85,14 @@ if __name__ == "__main__":
     #TODO: one word prompts don't work as well as short phrases. also, letters like "A" or "B" are not understood. also, "right" is not picked up very well.
     #Considering a different speech recognizer, or training our own classifier that classifies specifically into the predetermined commands ("right", "left", etc.)
 
+    game_info = {
+        "game": "Pokemon",
+        "available_commands": ["right", "left", "up", "down"]
+    }
+
     while True:
         print('Accepting new input:')
-        guess = recognize_speech_from_mic(recognizer, microphone)
+        guess = recognize_speech_from_mic(recognizer, microphone, game_info["available_commands"])
 
         if guess["error"]:
             print("ERROR: {}".format(guess["error"]))
