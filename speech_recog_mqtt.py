fdf8435086bb789b824300ab35897f03ee071cad
nturner0 <nturner1@g.ucla.edu>
2024-03-17 22:43:31 -0700

    added mqtt speech recognition

--- speech_recog_mqtt.py
+++ speech_recog_mqtt.py
@@ -13,14 +13,16 @@ def on_disconnect(client, userdata, rc):
     else:
         print('Expected Disconnect')
 
+def on_message(client, userdata, message):
+    print("Received message")
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
+client = mqtt.Client()
 
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
+client.on_message = on_message
 
 client.connect_async('mqtt.eclipseprojects.io')
-client.connect("mqtt.eclipse.org")
 
 client.loop_start()
 
@@ -35,7 +37,11 @@ def extract_command(word_list, keywords):
                 if (keyword in word_said.lower()):
                     count[i] += 1
     max_val = max(count)
-    return None if max_val == 0 else keywords[count.index(max_val)][0]
+
+    if max_val == 0:
+        return None
+    else:
+        return (keywords[count.index(max_val)][0], count.index(max_val))
 
 #Takes in a recognizer and microphone object, and a list of strings of valid commands
 #Returns a response dictionary
@@ -70,11 +76,9 @@ def recognize_speech_from_mic(recognizer, microphone, available_commands):
     response = {
         "success": True,
         "error": None,
-        "transcription": None
+        "transcription": None,
+        "command_index": 0
     }
-
-    #TODO: could try starting listening at a exact given time, instead of when the microphone energy passes the threshold
-    #TODO: look into Snowboy to use a hotword instead of the recgonizer. since we only have one word commands, this may be a useful alternative.
 
     # try recognizing the speech in the recording
     # if a RequestError or UnknownValueError exception is caught,
@@ -93,12 +97,14 @@ def recognize_speech_from_mic(recognizer, microphone, available_commands):
     #process the transcription
     if (raw_words):
         #remove all words except keyword commands
-        command = extract_command(raw_words.split(), available_commands)
+        command, command_index = extract_command(raw_words.split(), available_commands)
         #take the mode of the commands, and replace transcription with the command
         if (command):
             response["transcription"] = command
+            response["command_index"] = command_index
         else:
             response["error"] = "Words detected, but no commands. Original input was:\n" + raw_words
+
 
     return response
 
@@ -108,25 +114,39 @@ if __name__ == "__main__":
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
 
-    #TODO: one word prompts don't work as well as short phrases. also, letters like "A" or "B" are not understood. also, "right" is not picked up very well.
-    #Considering a different speech recognizer, or training our own classifier that classifies specifically into the predetermined commands ("right", "left", etc.)
-
-    game_info = {
-        "game": "Pokemon",
-        "available_commands": [("right", "riot", "write"), ("left", ), ("up", "pop"), ("down",), ("enter",), ("back",)]
+    game_info_pokemon = {
+        "name": "pokemon",
+        "available_commands": [("right", "riot", "write"), ("left", ), ("up", "pop"), ("down",), ("enter",), ("back",), ("select",)],
+        "send_commands": ("Right", "Left", "Up", "Down", "Enter", "Back", "Select")
     }
 
+    game_info_rps = {
+        "name": "rps",
+        "available_commands": [("rock",), ("paper",), ("scissors",)]
+    }
+
+    game_info_list = [game_info_pokemon, game_info_rps]
+
+    mode = "pokemon"
+
+    for game in game_info_list:
+        if (game["name"] == mode):
+            commands = game["available_commands"]
+            send_commands = game["send_commands"]
+
     while True:
-        input() #press enter in console to listen
+        #recognize_speech function waits 2 seconds while listening
         
         print('Accepting new input:')
-        guess = recognize_speech_from_mic(recognizer, microphone, game_info["available_commands"])
+        response = recognize_speech_from_mic(recognizer, microphone, commands)
 
-        if guess["error"]:
-            print("ERROR: {}".format(guess["error"]))
+        if response["error"]:
+            print("ERROR: {}".format(response["error"]))
         else:
-            print("You said: {}".format(guess["transcription"]))
-            print("Transmitting move to server...")
+            print("You said: {}".format(response["transcription"]))
+
+            print("Transmitting move to server...\n")
+            #client.publish("ece180d/test/turner", send_commands[response["command_index"]], qos=1)
 
 client.loop_stop()
 client.disconnect()
